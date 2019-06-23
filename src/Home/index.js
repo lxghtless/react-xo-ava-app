4a388b711f2ff3985c611269d770766410275dd8
lxghtlxss <lxghtl3ss@gmail.com>
2019-06-23 11:07:35 -0500

    initial work, tests not working

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import gravatarUrl from 'gravatar-url';
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div className="page">
-				Home page
+				<img
+					src={gravatarUrl('lxghtl3ss@gmail.com', {size: 100})}
+					style={{height: 100, width: 100}}
+					alt="@lxghtless"
+				/>
 			</div>
 		);
 	}
